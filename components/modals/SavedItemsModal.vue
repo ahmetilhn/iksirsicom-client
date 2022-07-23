9c24d023bf8b29c7063e441dae8b89a818caf768
Ahmet ilhan <ahmetilhan.dev@gmail.com>
2022-07-23 12:47:32 +0300

    SavedItemsModal cleaned

--- components/modals/SavedItemsModal.vue
+++ components/modals/SavedItemsModal.vue
@@ -1,40 +1,26 @@
 <template>
-  <modal-container :is-visible="isVisible"> </modal-container>
+  <modal-container :is-visible="isVisible" @click-outside="clickOutsideHandler">
+  </modal-container>
 </template>
 <script lang="js">
 
 import { mapState } from 'vuex';
 import ModalContainer from '../ModalContainer.vue'
-import lockScrollMixin from '~/mixins/lock-scroll.mixin';
 export default {
     name: "SavedItemsModal",
     components: {
         ModalContainer
     },
-    mixins: [lockScrollMixin],
     computed: {
         ...mapState({
             isVisible: store => store.modules.common.savedItemsModalIsVisible
         })
     },
-    watch: {
-        isVisible(newValue, _) {
-            this.lockScroll(newValue)
-        }
-    },
-    mounted() {
-        this.clickOutsideHandler();
-    },
     methods: {
-        clickOutsideHandler() {
-            const self = this;
-            window.addEventListener('click', function (e) {
-                if (e.target.id === 'modal_container') {
-                    self.$store.commit('modules/common/setSavedItemsModalVisible', false)
-                }
-            });
-        }
-    },
+      clickOutsideHandler(){
+        this.$store.commit('modules/common/setSavedItemsModalVisible', false)
+      }
+    }
 }
 </script>
 <style lang="scss" scoped></style>
