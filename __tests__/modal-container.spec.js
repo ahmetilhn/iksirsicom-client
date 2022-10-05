e04e8f71170a0d08aed28d0f26109fb23ce83ca9
Ahmet ilhan <ailhan@hepsiemlak.com>
2022-10-05 23:42:51 +0300

    Modal container test created

---
+++ __tests__/modal-container.spec.js
@@ -0,0 +1,26 @@
+import { mount } from '@vue/test-utils'
+import ModalContainer from '~/components/ModalContainer.vue'
+
+describe('ModalContainer.vue', () => {
+  const wrapper = mount(ModalContainer, {
+    props: {
+      type: Boolean,
+      default: false,
+    },
+  })
+  it('show with isVisible', async () => {
+    await wrapper.setProps({
+      isVisible: true,
+    })
+    const elem = wrapper.find('#modal_container')
+    expect(elem.exists()).toBeTruthy()
+  })
+
+  it('not show with isVisible', async () => {
+    await wrapper.setProps({
+      isVisible: false,
+    })
+    const elem = wrapper.find('.modal-container__content')
+    expect(elem.exists()).toBeFalsy()
+  })
+})
