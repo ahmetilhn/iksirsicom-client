3bec50f28e5461aa33247f68df2054b2c61343bd
Ahmet ilhan <ailhan@hepsiemlak.com>
2022-10-09 18:23:03 +0300

    base url changed

--- services/base.service.js
+++ services/base.service.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const baseHTTP = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: 'https://iksirsi-api.onrender.com',
   timeout: 8000,
   headers: { 'X-Custom-Header': 'iksirsi-api' },
 })
