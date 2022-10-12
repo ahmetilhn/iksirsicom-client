981d8f3621040493f9581a30ea44f090f36924e0
Ahmet ilhan <ailhan@hepsiemlak.com>
2022-10-12 18:52:58 +0300

    axios interceptor added(for res)

--- config/axios.config.js
+++ config/axios.config.js
@@ -5,5 +5,22 @@ const baseHTTP = axios.create({
   timeout: 8000,
   headers: { 'X-Custom-Header': 'iksirsi-api' },
 })
-
+baseHTTP.interceptors.response.use(
+  (res) => {
+    // Status controller
+    if (res.status) {
+      if (res.status >= 500 && res.status <= 599) {
+        console.error('Server Error', res.data)
+      } else if (res.status >= 400 && res.status <= 499) {
+        console.error('Client Error', res.data)
+      } else if (res.status >= 100 && res.status <= 199) {
+        console.info(res.data)
+      }
+    }
+    return res
+  },
+  (error) => {
+    return Promise.reject(error)
+  }
+)
 export default baseHTTP
