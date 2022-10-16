31b6fa954d80da078e0541b3190b771e1713d733
Ahmet ilhan <ailhan@hepsiemlak.com>
2022-10-17 00:22:56 +0300

    post-content-html-mixin improved

--- mixins/post-content-html.mixin.js
+++ mixins/post-content-html.mixin.js
@@ -9,21 +9,28 @@ export default {
           // For code
           const preElem = document.createElement('pre')
           switch (tagName) {
-            case 'p' || 'span' || 'a':
-              htmlElem.innerHTML = elem.text
+            case 'p':
+              htmlElem.innerText = elem.text
+              break
+            case 'span':
+              htmlElem.innerText = elem.text
               htmlElem.classList.add('no-code')
+              break
+            case 'a':
+              htmlElem.innerText = elem.text
+              htmlElem.href = elem.href
               break
             case 'img':
               htmlElem.src = elem.img_source
               break
             case 'code':
               htmlElem.classList.add(`language-${elem.lang}`)
-              htmlElem.innerHTML = elem.text.replace(/<br\/>/g, '\n')
+              htmlElem.innerText = elem.text.replace(/<br\/>/g, '\n')
               preElem.appendChild(htmlElem)
               htmlElem = preElem
               break
             default:
-              htmlElem.innerHTML = elem.text
+              htmlElem.innerText = elem.text
               break
           }
           inclusiveElem.appendChild(htmlElem)
