fed23a6973994c6b0c3a200f1ef1cda4e91bb0aa
Ahmet ilhan <ailhan@hepsiemlak.com>
2022-10-18 17:42:53 +0300

    header author component and test improved

--- __tests__/header-author.spec.js
+++ __tests__/header-author.spec.js
@@ -4,8 +4,12 @@ import HeaderAuthor from '~/components/HeaderAuthor.vue'
 describe('HeaderAuthor.vue', () => {
   const wrapper = mount(HeaderAuthor)
   it('has p and strong tag', () => {
-    const elem = wrapper.find('.author > p')
+    const elem = wrapper.find('.author > a > strong')
     expect(elem.exists).toBeTruthy()
-    expect(elem.text()).toContain('Ahmet İlhan')
+    expect(elem.text()).toContain('Destek Ol')
+  })
+  it('check visible svg icon', () => {
+    const svgElem = wrapper.find('.author > a > svg')
+    expect(svgElem.exists()).toBeTruthy()
   })
 })
